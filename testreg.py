3af1afc406a0bd112b5af368cb011dc7d24101cc
Alberto Rigail <albertojrigail@hotmail.com>
2021-02-18 19:45:20 -0500

    testing

---
+++ testreg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# testreg.py
+# Author: Alberto Rigail
+# Description: this program tests reg.py,
+#              and creates the coverage report 
+#-----------------------------------------------------------------------
+
+from os import system
+from sys import argv, stderr, exit
+
+#-----------------------------------------------------------------------
+def testReg(flags):
+    args = "-h"
+    # reference test.reg
+    system("python reg.py" + args + '> out1 2> &1')
+    # student test.reg
+    system("python /u/cos333/Asgt1Solution/ref_reg.pyc" + args + '> out2 2> &1')
+    system("diff -y out1 out2")
+
+def main(argv):       
+    testReg("-h")
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main(argv)
