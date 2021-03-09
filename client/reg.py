bed6d8ec9d1c5b14d295f0180cf88554f273fa81
Alberto Rigail <albertojrigail@hotmail.com>
2021-03-08 21:21:00 -0500

    client

--- client/reg.py
+++ client/reg.py
@@ -9,7 +9,7 @@ from sys import argv, stderr, exit
 from request import sendRequest
 import argparse
 from PyQt5.QtWidgets import QApplication, QFrame, QLabel
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QDesktopWidget, QListWidget, QScrollBar, QListWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QDesktopWidget, QListWidget, QScrollBar, QListWidgetItem, QMessageBox
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
@@ -19,57 +19,59 @@ REQUEST_COURSES_COMMAND = "getOverviews"
 REQUEST_CLASS_DETAILS_COMMAND = "getDetail"
 
 #-----------------------------------------------------------------------
+
 # this function makes a list widget, amnd calls 
-def getListWidget(host, port, arguments):
-    # create list widget, and add courses that match
-    listWidget = QListWidget()
-    courses = sendRequest(host, port, REQUEST_COURSES_COMMAND, arguments)
-
-    # set font
-    font = QFont('Courier', 10) 
-
-    # fill up list with courses
-    if courses is not None:
-        for course in courses:
-            currentItem = QListWidgetItem(course)
-            currentItem.setFont(font)
-            listWidget.addItem(currentItem)
-
-    # set scrollbars
-    verticalScrollbar = QScrollBar()
-    horizontalScrollbar = QScrollBar()
-    listWidget.setVerticalScrollBar(verticalScrollbar) 
-    listWidget.setHorizontalScrollBar(horizontalScrollbar) 
-
-    # set alignment
-    listWidget.setItemAlignment(Qt.AlignLeft)
-    
-    return listWidget
-
 
 
 def getDetailsPopup(host, port, classId):
     return None
 
 def main(argv):
-    # set up argparse with optional arguments
+    # PARSE ARGUMENTS
     parser = argparse.ArgumentParser(description="Client for the registrar application")
     parser.add_argument('host', type=str, help="the host on which the server is running", nargs=1)
     parser.add_argument('port', type=int, help="the port at which the server is listening", nargs=1)
-
-    # parse arguments
     arguments = parser.parse_args(argv[1:])
     host = arguments.host[0]
     port = arguments.port[0]
     
-    # graphical programming
+    # CREATE INITIAL INTERFACE
     app = QApplication(argv)
-
     layout = QVBoxLayout()
     layout.setSpacing(0)
     layout.setContentsMargins(0, 0, 0, 0)
 
-    listWidget = getListWidget(host, port, "")
+
+    # CREATE LIST WITH COURSES
+    listWidget = QListWidget()
+    font = QFont('Courier', 10) 
+    verticalScrollbar = QScrollBar()
+    horizontalScrollbar = QScrollBar()
+    listWidget.setVerticalScrollBar(verticalScrollbar) 
+    listWidget.setHorizontalScrollBar(horizontalScrollbar) 
+    listWidget.setItemAlignment(Qt.AlignLeft)
+
+    # on double click, show class details
+    def ItemDobleClicked():
+        item = listWidget.currentItem()
+        text = str(item.text())
+        row = text.split(' ')
+        classId = row[0]
+        classDetails = sendRequest(host, port, REQUEST_CLASS_DETAILS_COMMAND, "")
+        reply = QMessageBox.information(window, "Class Information", classDetails)
+
+
+   # fill up list with courses
+    courses = sendRequest(host, port, REQUEST_COURSES_COMMAND, "")
+    if courses is not None:
+        for course in courses:
+            currentItem = QListWidgetItem(course)
+            currentItem.setFont(font)
+            listWidget.itemDoubleClicked.connect(ItemDobleClicked(host, port, listWidget))
+            listWidget.addItem(currentItem)
+
+
+    # add list widget to layout
     layout.addWidget(listWidget)
 
     frame = QFrame()
