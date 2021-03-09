fdb38e9428a68bdacaf14318d20b9d9256fa91ec
Alberto Rigail <albertojrigail@hotmail.com>
2021-03-09 01:04:04 -0500

    python

--- server/regserver.py
+++ server/regserver.py
@@ -61,10 +61,10 @@ def main(argv):
                 sock.close()
                 print('Closed socket')
             except Exception as e:
-                print(argv[0] + ": " + e, file=stderr)
+                print(argv[0] + ":", e, file=stderr)
 
     except Exception as e:
-        print(e, file=stderr)
+        print(argv[0] + ":", e, file=stderr)
         exit(1)
 
 #-----------------------------------------------------------------------
