e045e0f2d2ae334823a360dfd3d56c7578845dfa
Alberto Rigail <albertojrigail@hotmail.com>
2021-04-11 20:51:41 -0400

    final changes to assignment 5

--- regserver.py
+++ regserver.py
@@ -6,6 +6,7 @@
 #-----------------------------------------------------------------------
 from sys import exit, argv, stderr
 from database import Database
+from course import Course
 from constants import *
 
 #Flask-specific imports
@@ -26,27 +27,7 @@ def index():
     title = request.args.get('title')
     arguments = [dept, num, area, title]
 
-    isError = False
-    errorMsg = ""
-
-    # database
-    courses=None
-    try:
-        db = Database()
-        db.connect()
-        courses = db.searchCourses(arguments)
-        db.disconnect()
-
-    except Exception as e:
-        isError = True
-        error = str(e)
-        # no class id exists
-        errorMsg = "A server error occurred. Please contact the system administrator."
-        if error == 'database reg.sqlite not found':
-            print('runserver.py: database reg.sqlite not found', file=stderr)
-        else:
-            print('runserver.py: ' + error, file=stderr)
-
+    # validation
     if dept is None:
         dept = ""
     if num is None:
@@ -55,22 +36,15 @@ def index():
         area = ""
     if title is None:
         title =""
+    
     # pass the arguments to jinja2
     html = render_template('templates/index.html',
-        courses=courses,
         dept=dept,
         num=num,
         area=area,
         title=title,
-        isError=isError,
-        errorMsg=errorMsg,
     )
-    response = make_response(html)
-    response.set_cookie('prevDept', dept)
-    response.set_cookie('prevNum', num)
-    response.set_cookie('prevArea', area)
-    response.set_cookie('prevTitle', title)
-    return response
+    return make_response(html)
     
 #-----------------------------------------------------------------------
 
@@ -118,30 +92,52 @@ def regdetails():
                     print("runserver.py: " + error, file=stderr)
             
 
-    # get cookies
-    prevDept = request.cookies.get('prevDept')
-    if prevDept is None:
-        prevDept = ""
-    prevNum = request.cookies.get('prevNum')
-    if prevNum is None:
-        prevNum = ""
-    prevArea = request.cookies.get('prevArea')
-    if prevArea is None:
-        prevArea = ""
-    prevTitle = request.cookies.get('prevTitle')
-    if prevTitle is None:
-        prevTitle = ""
-
     # pass the arguments to Jinja2 and render template,
     html = render_template('templates/regdetails.html',
         classDetails=classDetails,
         isError=isError,
         errorMsg=errorMsg,
-        prevDept=prevDept,
-        prevNum=prevNum,
-        prevArea=prevArea,
-        prevTitle=prevTitle,
         classId=classId,
     )
     response = make_response(html)
     return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/courses', methods=['GET'])
+def courses():
+    dept = request.args.get('dept')
+    num = request.args.get('num')
+    area = request.args.get('area')
+    title = request.args.get('title')
+    arguments = [dept, num, area, title]
+
+    courses = None
+    html = ''
+    try:
+        db = Database()
+        db.connect()
+        courses = db.searchCourses(arguments)
+        db.disconnect()
+
+        # format the courses in html
+        for course in courses:
+            html += "<tr>"
+            classId = course.getClassId() 
+            html += "<td><a href=regdetails?classid=" + classId + " target='_blank'>" + classId + "</a></td>"
+            html += "<td>" + course.getDept() + "</td>"
+            html += "<td>" + course.getCrsNum() + "</td>"
+            html += "<td>" + course.getArea() + "</td>"
+            html += "<td>" + course.getTitle() + "</td>"
+            html += "</tr>"
+    except Exception as e:
+        error = str(e)
+        # no class id exists
+        errorMsg = "A server error occurred. Please contact the system administrator."
+        html += '<div>' + errorMsg + '</div>'
+        if error == 'database reg.sqlite not found':
+            print('runserver.py: database reg.sqlite not found', file=stderr)
+        else:
+            print('runserver.py: ' + error, file=stderr)
+    
+    return  make_response(html)
